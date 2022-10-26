46e65117dd58787c84355b8f3c1c3469436557ab
Tina Chia <tinachia_5@yahoo.com>
2022-10-26 01:32:52 +0000

    draft array-manipulation-2

---
+++ array-manipulation-2/src/take-right.js
@@ -0,0 +1,8 @@
+/* exported takeRight */
+function takeRight(array, count) {
+  var arr = [];
+  for (var i = array.length + count; i < array.length; i++) {
+    arr.push(array[i]);
+  }
+  return arr;
+}
---
+++ array-manipulation-2/src/take.js
@@ -0,0 +1,8 @@
+/* exported take */
+function take(array, count) {
+  var arr = [];
+  for (var i = 0; i < count; i++) {
+    arr.push(array[i]);
+  }
+  return arr;
+}
