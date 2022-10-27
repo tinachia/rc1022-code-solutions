c92ea735a913ad8e7ab0e24c868e9d38522a8ada
Tina Chia <tinachia_5@yahoo.com>
2022-10-27 05:59:50 +0000

    string-manipulation-2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,7 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var sepWords = string.split(' ');
+  var firstLetter = string.charAt(0).toUpperCase(sepWords);
+  var capWords = firstLetter;
+  return capWords;
+}
