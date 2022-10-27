c6914693d4426e43a146258c9891159868d8aa34
Tina Chia <tinachia_5@yahoo.com>
2022-10-27 18:47:08 +0000

    draft string-manipulation-2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,7 +1,10 @@
 /* exported capitalizeWords */
-function capitalizeWords(string) {
-  var sepWords = string.split(' ');
-  var firstLetter = string.charAt(0).toUpperCase(sepWords);
-  var capWords = firstLetter;
-  return capWords;
-}
+// function capitalizeWords(string) {
+//   var lowCase = string.charAt(0).toLowerCase();
+//   var splitWords = string.split(' ');
+//   var firstLetter = string.charAt(0).toUpperCase(splitWords);
+//   var capCase = firstLetter;
+//   return capCase;
+// }
+
+// console.log(capitalizeWords('needs solution'));
