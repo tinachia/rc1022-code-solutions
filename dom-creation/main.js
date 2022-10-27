e9354ee65d79541a6064e5ee48ea5709ba8bbb10
Tina Chia <tinachia_5@yahoo.com>
2022-10-27 16:22:02 +0000

    completed dom-creation

--- dom-creation/main.js
+++ dom-creation/main.js
@@ -57,45 +57,55 @@ var pokedex = [
   }
 ];
 
-// function renderPokemon(pokemon) {
-//   var newElementFirst = document.createElement('div');
-//   newElementFirst.setAttribute('class', 'column-third');
+function renderPokemon(pokemon) {
+  var newElementFirst = document.createElement('div');
+  newElementFirst.setAttribute('class', 'column-third');
 
-//   var newElementSecond = document.createElement('div');
-//   newElementSecond.setAttribute('class', 'pokemon-card');
-//   var secondClass = row.appendChild(newElementFirst);
-//   secondClass.appendChild(newElementSecond);
+  var newElementSecond = document.createElement('div');
+  newElementSecond.setAttribute('class', 'pokemon-card');
+  newElementFirst.appendChild(newElementSecond);
 
-//   var newElementThird = document.createElement('img');
-//   newElementThird.setAttribute('src', pokemon.imageUrl);
-//   newElementSecond.appendChild(newElementThird);
+  // var secondClass = row.appendChild(newElementFirst);
+  // secondClass.appendChild(newElementSecond);
 
-//   var newElementFourth = document.createElement('div');
-//   newElementFourth.setAttribute('class', 'pokemon-card-text');
-//   newElementSecond.appendChild(newElementFourth);
+  var newElementThird = document.createElement('img');
+  newElementThird.setAttribute('src', pokemon.imageUrl);
+  newElementSecond.appendChild(newElementThird);
+  // newElementSecond.appendChild(newElementThird);
 
-//   var newElementFifth = document.createElement('h2');
-//   var name = document.createTextNode(pokemon.name);
-//   newElementFifth.appendChild(name);
-//   newElementFourth.appendChild(newElementFifth);
+  var newElementFourth = document.createElement('div');
+  newElementFourth.setAttribute('class', 'pokemon-card-text');
+  newElementSecond.appendChild(newElementFourth);
+  // newElementSecond.appendChild(newElementFourth);
 
-//   var newElementSixth = document.createElement('h3');
-//   var number = document.createTextNode(pokemon.number);
-//   newElementSixth.appendChild(number);
-//   newElementFourth.appendChild(newElementSixth);
+  var newElementFifth = document.createElement('h2');
+  newElementFifth.textContent = pokemon.name;
+  newElementFourth.appendChild(newElementFifth);
+  // var name = document.createTextNode(pokemon.name);
+  // newElementFifth.appendChild(name);
+  // newElementFourth.appendChild(newElementFifth);
 
-//   var newElementSeventh = document.createElement('p');
-//   var parag = document.createTextNode(pokemon.description);
-//   newElementSeventh.appendChild(parag);
-//   newElementFourth.appendChild(newElementSeventh);
+  var newElementSixth = document.createElement('h3');
+  newElementSixth.textContent = '#' + pokemon.number;
+  newElementFourth.appendChild(newElementSixth);
+  // var number = document.createTextNode(pokemon.number);
+  // newElementSixth.appendChild(number);
+  // newElementFourth.appendChild(newElementSixth);
 
-//   return newElementFirst;
+  var newElementSeventh = document.createElement('p');
+  newElementSeventh.textContent = pokemon.description;
+  newElementFourth.appendChild(newElementSeventh);
+  // var parag = document.createTextNode(pokemon.description);
+  // newElementSeventh.appendChild(parag);
+  // newElementFourth.appendChild(newElementSeventh);
 
-// }
+  return newElementFirst;
 
-// var row = document.querySelector('.row');
+}
 
-// // for (var i = 0; i < pokedex.length; i++) {
-// //   return
-// // }
-// // return renderPokemon;
+var $row = document.querySelector('.row');
+
+for (var i = 0; i < pokedex.length; i++) {
+  var $pokemon = renderPokemon(pokedex[i]);
+  $row.appendChild($pokemon);
+}
