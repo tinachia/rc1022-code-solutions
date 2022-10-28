b3e64a91e67f44cfbe153ae944b4a4d929f64ab5
Tina Chia <tinachia_5@yahoo.com>
2022-10-28 19:35:52 +0000

    completed javascript-local-storage

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -2,9 +2,11 @@
 
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-todos = JSON.parse(previousTodosJSON);
 
 window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
+  if (previousTodosJSON !== null) {
+    todos = JSON.parse(previousTodosJSON);
+  }
 });
