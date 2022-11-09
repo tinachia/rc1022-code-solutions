1f36f0cd3d42a5b31318d489bfd5a9d067b385e9
Tina Chia <tinachia_5@yahoo.com>
2022-11-09 18:31:40 +0000

    completed javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -7,15 +7,16 @@ var count = 4;
 // define function countdown
 // decrement count
 // if statement, when count hit 0, update textContent
+// clear the count with clearInterval
 function countDown() {
   count--;
   $h1.textContent = count;
   if (count <= 0) {
     $h1.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(counter);
   }
 
 }
 // setInterval passing in callback function and set it for every second
-// clear the count with clearInterval
-setInterval(countDown, 1000);
-clearInterval(countDown);
+
+var counter = setInterval(countDown, 1000);
