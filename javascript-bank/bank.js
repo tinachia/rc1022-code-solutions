eee5a77cf9bc3e3092b30e5ca530cd5f87685126
Tina Chia <tinachia_5@yahoo.com>
2022-11-09 01:12:02 +0000

    completed javascript-bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,39 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance <= 0 || Number.isInteger(balance) === false) {
+    return null;
+  } else {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  }
+
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (number === this.accounts[i].number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var grandTotal = 0;
+  if (this.accounts.length === 0) {
+    return grandTotal;
+  } else {
+    for (var i = 0; i < this.accounts.length; i++) {
+      grandTotal += this.accounts[i].getBalance();
+    }
+  } return grandTotal;
+};
