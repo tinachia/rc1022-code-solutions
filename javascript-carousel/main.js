5779dd82aa1547705aa5e0c9d57e7e6f46cef12e
Tina Chia <tinachia_5@yahoo.com>
2022-11-10 00:41:31 +0000

    completed carousel function

---
+++ javascript-carousel/main.js
@@ -0,0 +1,37 @@
+// query select all classes
+// var $arrows = document.querySelector('.arrows');
+// var $leftArrow = document.querySelector('.fa-chevron-left');
+// var $pokemonImages = document.querySelector('.pokemon');
+// var $rightArrow = document.querySelector('.fa-chevron-right');
+// var $circle = document.querySelector('.circles');
+// var $regularCircle = document.querySelector('.fa-circle');
+// // declare a var counter with value of 0
+// var counter = 0;
+// // declare and var id with value of null
+// var id = null;
+// define a function for carousel
+// use classList method to add 'hidden' to images
+// use className to update to an open circle
+// increment counter by 1
+
+// if the counter reach the length of the images, restart counter at 0
+
+// use classList method to remove 'hidden' to images
+// use className to update to a solid circle
+// function carousel() {
+//   $pokemonImages[counter].classList.add('hidden');
+//   $regularCircle[counter].className = 'fa-regular fa-circle';
+//   counter++;
+//   if (counter === $pokemonImages.length) {
+//     counter = 0;
+//   }
+//   $pokemonImages[counter].classList.remove('hidden');
+//   $regularCircle[counter].className = 'fa-solid fa-circle';
+// }
+
+// setInterval of carousel to 3 secs
+
+// add event listener for click on the arrows
+
+// create a function that loops through the arrows
+// if
