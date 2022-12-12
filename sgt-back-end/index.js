04de71996f61eaba3bb6cff0850d5fdeff1bb1f9
Tina Chia <tinachia_5@yahoo.com>
2022-12-12 17:02:53 +0000

    draft sgt-back-end

---
+++ sgt-back-end/index.js
@@ -0,0 +1,9 @@
+const pg = require('pg');
+
+// only create ONE pool for your whole server
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
